962775858a3d270c44434f1d1ca7854a0df4ce4f
MatrixDoge <xywhpanda@gmail.com>
2018-10-18 22:09:03 -0400

    Initial commit

---
+++ SolveMatrixLeaseSquare.py
@@ -0,0 +1,17 @@
+import numpy
+import fractions
+numpy.set_printoptions(formatter={'all':lambda x: str(fractions.Fraction(x).limit_denominator())})
+
+def get_least_square(mInput, mResult):
+    mInputTranspose = numpy.transpose(mInput)
+    mAtA = numpy.matmul(mInputTranspose, mInput)
+    mAtAInv = numpy.linalg.inv(mAtA)
+    mAtR = numpy.matmul(mInputTranspose, mResult)
+    mBetaHat = numpy.matmul(mAtAInv, mAtR)
+    return mBetaHat
+
+
+
+a = numpy.array([[2, 1], [1, -1], [1, 1]])
+r = numpy.array([[3], [1], [2]])
+print(get_least_square(a,r))
